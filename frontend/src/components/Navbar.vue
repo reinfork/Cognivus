bd4a4e1632b7c53a3a91bce9dbc61df65013586a
dwipapap <dwipahyaki@gmail.com>
2025-08-30 10:18:24 +0700

    frontend: landing: add simple landing page

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<script setup>
+</script>
+
+<template>
+  <div class="navbar bg-base-100 shadow-sm sticky top-0 z-50">
+    
+    <div class="navbar-start">
+      <a href="ittrenglishcourse.com" class="btn btn-ghost text-xl">
+        ITTR ENGLISH
+      </a>
+    </div>
+
+    <div class="navbar-center">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>Home</a></li>
+        <li><a>Article</a></li>
+        <li><a>Programs</a></li>
+        <li><a>Testimonials</a></li>
+        <li><a>About Us</a></li>
+        <li>
+          <details>
+            <summary>Parent</summary>
+            <ul class="bg-base-100 rounded-t-none p-2">
+              <li><a>Link 1</a></li>
+              <li><a>Link 2</a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </div>
+<div>
+  
+
+</div>
+    <div class="navbar-end gap-3">
+        <button class="btn rounded-3xl btn-soft btn-primary pr-5">Masuk</button>
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+    </div>
+      </div>
+
+</template>
+
+<style scoped>
+</style>
