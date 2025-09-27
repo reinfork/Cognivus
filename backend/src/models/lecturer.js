26a41c2f124a8180531eae6b6b7a4094249d858f
reinfork <skynaufal738@gmail.com>
2025-09-27 22:44:36 +0700

    redefine tables columns in models folder

--- backend/src/models/lecturer.js
+++ backend/src/models/lecturer.js
@@ -1,12 +1,18 @@
 // This is a representation of your Supabase table
 // In Supabase, you don't need Sequelize models, but this helps with documentation
 
-class Lecturer {
+class tbTeacher {
   constructor(data) {
     this.user_id = data.id;
-    this.username = data.username;
-    this.password_hash = data.password_hash;
+    this.fullname = data.fullname;
+    this.birthplace = data.birthplace;
+    this.birthdate = data.birthdate;
+    this.address = data.address;
+    this.phone = data.phone;
+    this.lasteducation = data.lasteducation;
+    this.gender = data.gender;
     this.email = data.email;
+    this.photo = data.photo;
   }
 }
 
--- backend/src/models/student.js
+++ backend/src/models/student.js
@@ -1,16 +1,23 @@
 // This is a representation of your Supabase table
 // In Supabase, you don't need Sequelize models, but this helps with documentation
 
-class Student {
+class tbStudent {
   constructor(data) {
     this.user_id = data.id;
     this.student_id = data.student_id;
-    this.nama_lengkap = data.nama_lengkap;
-    this.jenis_kelamin = data.jenis_kelamin;
-    this.alamat = data.alamat;
-    this.no_hp = data.no_hp;
-    this.nama_ortu = data.nama_ortu;
-    this.no_hp_ortu = data.no_hp_ortu;
+    this.fullname = data.fullname;
+    this.gender = data.gender;
+    this.address = data.address;
+    this.phone = data.phone;
+    this.parentname = data.parentname;
+    this.parentphone = data.parentphone;
+    this.birthplace = data.birthplace;
+    this.birthdate = data.birthdate;
+    this.photo = data.photo;
+    this.created_at = data.created_at;
+    this.birthdate = data.birthdate;
+    this.created_at = data.created_at;
+    this.updated_at = data.updated_at;
   }
 }
 
